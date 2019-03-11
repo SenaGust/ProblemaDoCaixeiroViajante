126817a55325d9e8f89bf633c5bbe50d106f610c
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-11 12:03:30 -0300

    implementação dos métodos preencher_matriz e imprimir_matriz

--- Problema do Caixeiro Viajante/Program.cs
+++ Problema do Caixeiro Viajante/Program.cs
@@ -12,8 +12,37 @@ namespace Problema_do_Caixeiro_Viajante
     {
         static void Main(string[] args)
         {
-            int n=3;
-            int [,] mat= new int[n,n]; 
+            int[,] matriz;
+            int tamanho = 4;
+            matriz = new int[tamanho, tamanho];
+
+            Preencher_Matriz(matriz);
+            Imprimir_Matriz(matriz);
+
+            //Fim
+            Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+        static void Preencher_Matriz(int[,] mat)
+        {
+            for (int linha = 0; linha < mat.GetLength(0); linha++)
+                for (int coluna = 0; coluna < mat.GetLength(1); coluna++)
+                    if (linha == coluna)
+                        mat[linha, coluna] = 0;
+                    else
+                        mat[linha, coluna] = linha + coluna + 1;
+        }
+        static void Imprimir_Matriz(int[,] matrizFrase)
+        {
+            //para testes
+            for (int linha = 0; linha < matrizFrase.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matrizFrase.GetLength(1); coluna++)
+                {
+                    Console.Write(matrizFrase[linha, coluna]+ " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

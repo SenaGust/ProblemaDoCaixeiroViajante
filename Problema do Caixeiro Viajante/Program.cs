3d72a978b21a0a2bc77d3e22d9899f9cddeec320
loriAguilar <44687575+loriAguilar@users.noreply.github.com>
2019-03-11 11:42:14 -0300

    matriz

--- Problema do Caixeiro Viajante/Program.cs
+++ Problema do Caixeiro Viajante/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//criar arquivo csv
+//O(n!)= (n-1!)
 namespace Problema_do_Caixeiro_Viajante
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int n=3;
+            int [,] mat= new int[n,n]; 
         }
     }
 }

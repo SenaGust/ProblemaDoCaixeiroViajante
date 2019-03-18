a85f6337910f102a75979ad5335f39499390620c
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-18 15:23:48 -0300

    Implementação inicial de atributos, construtor e métodos

--- Problema do Caixeiro Viajante/Forca_bruta.cs
+++ Problema do Caixeiro Viajante/Forca_bruta.cs
@@ -8,5 +8,42 @@ namespace Problema_do_Caixeiro_Viajante
 {
     class Forca_bruta
     {
+        Trajeto[] todosTrajetos;
+        int[,] matrizDistancias;
+
+        public Forca_bruta(int[,] matrizDistancias)
+        {
+            this.matrizDistancias = matrizDistancias; //acha que isso é necessário?
+            todosTrajetos = new Trajeto[Fatorial(matrizDistancias.GetLength(0))]; //fatorial do total de linhas/colunas (que é a quantidade total de cidades)
+        }
+
+        public void RetornaMenorDistancia()
+        {
+            PermutarCidades();
+            DescobreTrajetoMenorDistancia();
+        }
+
+        private void PermutarCidades()
+        {
+
+        }
+
+        private void DescobreTrajetoMenorDistancia()
+        {
+            
+        }
+
+        #region Métodos Auxiliares
+        private int Fatorial(int numero)
+        {
+            int total = 1;
+            for (int pos = 2; pos <= numero; pos++)
+            {
+                total *= total;
+            }
+            return total;
+        }
+        #endregion
     }
+    
 }

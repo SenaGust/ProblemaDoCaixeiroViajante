c876ae2448a975ad1d06bd5a28b7a739e2570bf0
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-21 17:35:47 -0300

    Escolha do método ToString()

--- Problema do Caixeiro Viajante/Trajeto.cs
+++ Problema do Caixeiro Viajante/Trajeto.cs
@@ -22,25 +22,10 @@ namespace Problema_do_Caixeiro_Viajante
         #endregion
 
         #region Métodos
-        public string ToString2()
+        public override string ToString()
         {
             //retorna assim "O caminho abc possui a distância 10."
             return "O caminho " + new string(caminho) + " possui a distância " + distancia + ".";
-        }
-
-        public override string ToString()
-        {
-            //retorna assim "O caminho a b c possui a distância 10."
-            StringBuilder stringBuilder = new StringBuilder();
-
-            stringBuilder.Append("O caminho ");
-            for (int pos = 0; pos < caminho.Length; pos++)
-            {
-                stringBuilder.Append(caminho[pos] + " ");
-            }
-            stringBuilder.Append(" possui a distância " + distancia + ".");
-
-            return stringBuilder.ToString();
         }
         #endregion
     }

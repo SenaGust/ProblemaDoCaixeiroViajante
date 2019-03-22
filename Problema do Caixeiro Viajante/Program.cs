bb8a6b76861a73ae98f0a87e36c5cc825d108218
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-22 11:21:31 -0300

    Ajuste

--- Problema do Caixeiro Viajante/Program.cs
+++ Problema do Caixeiro Viajante/Program.cs
@@ -10,54 +10,14 @@ namespace Problema_do_Caixeiro_Viajante
     {
         static void Main(string[] args)
         {
-            int quantidadeCidades = 4;
-            int[,] matrizDistanciaCidades = new int[quantidadeCidades, quantidadeCidades];
-
-            Preencher_Matriz(matrizDistanciaCidades);
-            Imprimir_Matriz(matrizDistanciaCidades);
+            int qtdeCidades = 4;
+            MatrizCidades matriz = new MatrizCidades(qtdeCidades);
+            matriz.Preencher();
+            matriz.Imprimir();
 
             //Fim
             Console.WriteLine("\n\nPressione qualquer tecla para continuar...");
             Console.ReadKey();
         }
-
-        #region Métodos para a Matriz
-        static void Preencher_Matriz(int[,] matriz)
-        {
-            int custo;
-            Random randomizer = new Random(); //criar distâncias 
-
-            for (int linha = 0; linha < matriz.GetLength(0); linha++)
-            {
-                for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
-                {
-                    custo = randomizer.Next(10) + 1;
-                    if (linha < coluna)
-                        matriz[linha, coluna] = custo;
-                    else
-                        if (linha == coluna)
-                        matriz[linha, coluna] = 0;
-                    else
-                        matriz[linha, coluna] = matriz[coluna, linha];
-                }
-            }
-            
-        }
-        #endregion
-
-        #region Métodos usados para teste
-        static void Imprimir_Matriz(int[,] matrizFrase)
-        {
-            //para testes
-            for (int linha = 0; linha < matrizFrase.GetLength(0); linha++)
-            {
-                for (int coluna = 0; coluna < matrizFrase.GetLength(1); coluna++)
-                {
-                    Console.Write(matrizFrase[linha, coluna] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-        #endregion
     }
 }

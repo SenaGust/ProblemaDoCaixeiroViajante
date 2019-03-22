97d3593c76efc605dc4b679c1a5aebd88018573f
Gustavo Sena <gustavosena98@LIVE.COM>
2019-03-22 11:20:03 -0300

    Implementação da classe

--- Problema do Caixeiro Viajante/MatrizCidades.cs
+++ Problema do Caixeiro Viajante/MatrizCidades.cs
@@ -8,5 +8,50 @@ namespace Problema_do_Caixeiro_Viajante
 {
     class MatrizCidades
     {
+        int[,] m;
+
+        #region Contrutor
+        public MatrizCidades(int quantidadeCidades)
+        {
+            m = new int[quantidadeCidades, quantidadeCidades];
+        }
+        #endregion
+
+
+        #region Métodos
+        public void Preencher()
+        {
+            int custo;
+            Random randomizer = new Random(); //criar distâncias 
+
+            for (int linha = 0; linha < m.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < m.GetLength(1); coluna++)
+                {
+                    custo = randomizer.Next(10) + 1;
+                    if (linha < coluna)
+                        m[linha, coluna] = custo;
+                    else
+                        if (linha == coluna)
+                        m[linha, coluna] = 0;
+                    else
+                        m[linha, coluna] = m[coluna, linha];
+                }
+            }
+
+        }
+        public void Imprimir()
+        {
+            //para testes
+            for (int linha = 0; linha < m.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < m.GetLength(1); coluna++)
+                {
+                    Console.Write(m[linha, coluna] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
     }
 }
